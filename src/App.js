f6816636436d28fe13ee0e631dafd63c167c6c80
nttminh-dev <mnguyen@bhcc.edu>
2021-11-26 16:00:58 -0500

    apply Router to App()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
-import instance from './axios';
-import Banner from './components/Banner';
-import Row from './components/Row';
-import requests from './requests';
-import Nav from './components/Nav';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 
 function App() {
+  const user = null;
+
   return (
     <div className="App">
-      <Nav />
-      <Banner />
-      <Row title='NETFLIX ORIGINALS' fetchURL={requests.fetchNetflixOriginals} isLargeRow />
-      <Row title='Trending Now' fetchURL={requests.fetchTrending} />
-      <Row title='Top Rated' fetchURL={requests.fetchTopRated} />
-      <Row title='Action Movies' fetchURL={requests.fetchActionMovies} />
-      <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
-      <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} />
-      <Row title='Documentaries' fetchURL={requests.fetchDocumentaries} />
+      <Router>
+        {!user ? <LoginScreen /> :
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        }
+      </Router>
     </div>
   );
 }
