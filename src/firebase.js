db741bcbed19257746ae031f90412a0947924a09
nttminh-dev <mnguyen@bhcc.edu>
2021-11-26 16:00:40 -0500

    config firebase backend

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDXqEPhIg8qrEqoG4qzM1FX1Dwf89CmUuk",
+    authDomain: "netflix-clone-2cd17.firebaseapp.com",
+    projectId: "netflix-clone-2cd17",
+    storageBucket: "netflix-clone-2cd17.appspot.com",
+    messagingSenderId: "657180026562",
+    appId: "1:657180026562:web:e9f215795ee52fed67bd5b",
+    measurementId: "G-79Q59SZ62Q"
+};
