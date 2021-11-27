fd09499d2e3c9250527a2820b8314265306f8921
nttminh-dev <mnguyen@bhcc.edu>
2021-11-26 21:24:56 -0500

    set up Redux and add Route for /profile page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
+      } else {
+        // Logged out
+        dispatch(logout)
+      }
+    })
+
+    // clean up the add event listener
+    return unsubscribe;
+  }, [])
 
   return (
     <div className="App">
       <Router>
         {!user ? <LoginScreen /> :
           <Switch>
+            <Route path="/profile" >
+              <ProfileScreen />
+            </Route>
+
             <Route exact path="/">
               <HomeScreen />
             </Route>
