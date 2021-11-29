0fdf5744d2efe36bcc81a124d2e6030b90071938
nttminh-dev <mnguyen@bhcc.edu>
2021-11-28 22:48:38 -0500

    Add toSignUp state, rerender component when state change

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { auth } from '../firebase';
 import './SignupScreen.css'
 
+
 const SignupScreen = () => {
+    const [toSignUp, setToSignUp] = useState(false);
 
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
@@ -37,20 +39,42 @@ const SignupScreen = () => {
             })
     }
 
+    const handleToggle = () => {
+        setToSignUp(!toSignUp);
+    }
+
+    useEffect(() => {
+        return () => {
+        }
+    }, [toSignUp])
+
     return (
         <div className='signupScreen'>
-            <form>
-                <h1>Sign In</h1>
-                <input ref={emailRef} placeholder='Email' type='email' />
-                <input ref={passwordRef} placeholder='Password' type='password' />
-                <button type='submit' onClick={signIn}>Sign In</button>
+            {!toSignUp ?
 
-                <h4>
-                    <span className='signupScreen_gray'>New to Netflix? </span>
-                    <span className='signupScreen_link' onClick={register}>Sign Up now.</span>
-                </h4>
-            </form>
+                <form>
+                    <h1>Sign In</h1>
+                    <input ref={emailRef} placeholder='Email' type='email' />
+                    <input ref={passwordRef} placeholder='Password' type='password' />
+                    <button type='submit' onClick={signIn}>Sign In</button>
 
+                    <h4>
+                        <span className='signupScreen_gray'>New to Netflix? </span>
+                        <span className='signupScreen_link' onClick={handleToggle}>Sign Up now.</span>
+                    </h4>
+                </form> :
+                <form>
+                    <h1>Sign Up</h1>
+                    <input ref={emailRef} placeholder='Email' type='email' />
+                    <input ref={passwordRef} placeholder='Password' type='password' />
+                    <button type='submit' onClick={register}>Sign Up</button>
+
+                    <h4>
+                        <span className='signupScreen_gray'>Had an account? </span>
+                        <span className='signupScreen_link' onClick={handleToggle}>Sign In now.</span>
+                    </h4>
+                </form>
+            }
         </div >
     )
 }
